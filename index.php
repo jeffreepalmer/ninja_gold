f7455a4aeab520628502f6484a56f09a677bff8e
Jeff Palmer <jeffreepalmer@gmail.com>
2015-01-23 10:22:19 -0800

    divs and forms in place. not much else.

---
+++ index.php
@@ -0,0 +1,29 @@
+Your Gold: 
+<div>
+</div>
+
+
+<div>
+	<form action='process.php' method='post'>
+		<input type='hidden' name='form_name' value='farm'>
+	</form>
+</div>
+<div>
+	<form action='process.php' method='post'>
+		<input type='hidden' name='form_name' value='cave'>
+	</form>
+</div>
+<div>
+	<form action='process.php' method='post'>
+		<input type='hidden' name='form_name' value='house'>
+	</form>
+</div>
+<div>
+	<form action='process.php' method='post'>
+		<input type='hidden' name='form_name' value='casino'>
+	</form>
+</div>
+
+Activities:
+<div>
+</div>
